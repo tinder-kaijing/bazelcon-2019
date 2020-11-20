45a3edd9041fcdfa8cebc762e24b1c134d0e4560
Kai Jing <kai.jing@gotinder.com>
2020-11-19 16:47:12 -0800

    to use bazelisk to replace bazel command

--- 2-java-tooling/exercise.sh
+++ 2-java-tooling/exercise.sh
@@ -5,15 +5,15 @@ set -e
 export MSYS_NO_PATHCONV=1
 export MSYS2_ARG_CONV_EXCL="*"
 
-bazel build '...'
-bazel test '...' --test_verbose_timeout_warnings --test_output=errors
-bazel run :hello-world
+bazelisk build '...'
+bazelisk test '...' --test_verbose_timeout_warnings --test_output=errors
+bazelisk run :hello-world
 
 # See a list of targets:
-# bazel query '...'
+# bazelisk query '...'
 
 # See a diagram of the those tarage (on a Mac with graphviz installed)
-# bazel query --output=graph '...' | dot -Tpng >temp.png ; open temp.png
+# bazelisk query --output=graph '...' | dot -Tpng >temp.png ; open temp.png
 
 # This is extraordinarily helpful in projects that require (correct,
 # i.e. generated) documentation of how things work.
--- 3-java-dependencies/exercise.sh
+++ 3-java-dependencies/exercise.sh
@@ -5,14 +5,14 @@ set -e
 export MSYS_NO_PATHCONV=1
 export MSYS2_ARG_CONV_EXCL="*"
 
-bazel build '...'
-bazel test '...' --test_verbose_timeout_warnings --test_output=errors
+bazelisk build '...'
+bazelisk test '...' --test_verbose_timeout_warnings --test_output=errors
 
-bazel run //app-one/src/main/java/com/example/myproject:hello-world
-bazel run //app-one/src/main/java/com/example/myproject:hello-data
+bazelisk run //app-one/src/main/java/com/example/myproject:hello-world
+bazelisk run //app-one/src/main/java/com/example/myproject:hello-data
 
 # See a list of targets:
-# bazel query '...'
+# bazelisk query '...'
 
 # See a diagram of the those tarage (on a Mac with graphviz installed)
-# bazel query --output=graph ... | dot -Tpng >temp.png ; open temp.png
+# bazelisk query --output=graph ... | dot -Tpng >temp.png ; open temp.png
