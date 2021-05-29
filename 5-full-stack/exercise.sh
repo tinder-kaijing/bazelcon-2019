e9eeeb3d1964bbb5b6b945fa1ee224eef54dd73e
tinder-kaijing <kai.jing@gotinder.com>
2021-05-28 19:03:50 -0700

    fix some error of scripts

--- 5-full-stack/exercise.sh
+++ 5-full-stack/exercise.sh
@@ -5,12 +5,12 @@ set -e
 export MSYS_NO_PATHCONV=1
 export MSYS2_ARG_CONV_EXCL="*"
 
-bazel build '...'
-bazel test '...' --test_verbose_timeout_warnings --test_output=errors
+bazelisk build '...'
+bazelisk test '...' --test_verbose_timeout_warnings --test_output=errors
 
-# bazel run //time-server
-# bazel run //fx-server
-# bazel run //client
+# bazelisk run //time-server
+# bazelisk run //fx-server
+# bazelisk run //client
 
 # http://localhost:5432/
 # http://localhost:8005/
